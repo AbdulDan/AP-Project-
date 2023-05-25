f5503063e5fac0a84f8b06a5c63c632ec0c957bd
EC2 Default User <ec2-user@ip-172-31-25-75.ec2.internal>
2023-05-25 18:20:12 +0000

    template values for initial vals, source, and observer

--- script.js
+++ script.js
@@ -1,15 +1,59 @@
-let spd = 0.25;
-const num = 250;
-const skp = 10;
-const prd = 20;
+const spd = //val;
+const num = //val;
+const skp = //val;
+const prd = //val;
 
 let time = 0;
 let view = 0;
 let plot = 0;
 
-let run = true;
+let run = //boo;
 let buft = null;
 let fmod = 0;
-let snd = false;
-let twav = false;
+let snd = //boo;
+let twav = //boo;
 let obj = null;
+
+let src = {
+    wav: null,
+    freq: //val,
+    amp: //val,
+    type: //val,
+    dir: //val,
+    mag: //val,
+    pos: {
+        x: //val,
+        y: //val
+    },
+    vel: {
+        x: //val,
+        y: //val
+    },
+    acc: {
+        x: //val,
+        y: //val
+    }
+};
+
+let obs = {
+    wav: null,
+    freq: null,
+    amp: null,
+    type: //val,
+    dir: //val,
+    mag: //val,
+    pos: {
+        x: //val,
+        y: //val
+    },
+    vel: {
+        x: //val,
+        y: //val
+    },
+    acc: {
+        x: //val,
+        y: //val
+    }
+};
+
+
