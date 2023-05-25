d89445ab68136dcaa1c07840032be62c22b2c5db
EC2 Default User <ec2-user@ip-172-31-25-75.ec2.internal>
2023-05-25 17:53:04 +0000

    initial varables for function

---
+++ script.js
@@ -0,0 +1,15 @@
+let spd = 0.25;
+const num = 250;
+const skp = 10;
+const prd = 20;
+
+let time = 0;
+let view = 0;
+let plot = 0;
+
+let run = true;
+let buft = null;
+let fmod = 0;
+let snd = false;
+let twav = false;
+let obj = null;
